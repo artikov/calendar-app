188d5cdf504c36d96dac52f07c315b3e393d6efc
Oybek Artikov <oybekartikov@icloud.com>
2023-01-16 14:00:31 +0500

    full-calendar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FullCalendarModule } from '@fullcalendar/angular';
+import { FullCalendarComponent } from './full-calendar/full-calendar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FullCalendarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
